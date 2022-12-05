d5baace7cd04a18bfd8cbcf6a1698ae21c0a91d5
Anja Ostovršnik <61201059+Anja159@users.noreply.github.com>
2022-12-05 07:59:48 +0100

    Day 5: Supply Stacks ❄️

---
+++ Day5/part2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+f = open("in.txt", "r")
+
+stack1 = []
+for i in range(10):
+    stack1.append([])
+
+for vrstica in f:
+    # Moving crates
+    if vrstica[0] == "m":
+        a = vrstica.split(" ")
+        temp1 = []
+        for r in range(0, int(a[1])):
+            element = stack1[int(a[3])].pop()
+            temp1.insert(0, element)
+        stack1[int(a[5])] = stack1[int(a[5])] + temp1
+
+    else:
+        # Putting crates onto the stack
+        parts = [vrstica[i : i + 4] for i in range(0, len(vrstica), 4)]
+        st = 0
+
+        if vrstica.find("[") == -1:
+            continue
+
+        for part in parts:
+            crka = part[1:2]
+            st += 1
+            if crka == " ":
+                continue
+            stack1[st].insert(0, crka)
+
+# Reading top elements
+for h in range(0, 10):
+    if len(stack1[h]):
+        print(stack1[h].pop(), end="")
